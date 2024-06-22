56a60ca1d14b44d4e3ae75fbba5b984f5e5594c4
Karabo Rithuri <rithurikarabo@gmail.com>
2024-06-22 17:16:58 +0200

    method to display the Pizza description and price

--- PizzaOrdering/Pizza.cs
+++ PizzaOrdering/Pizza.cs
@@ -15,7 +15,7 @@ namespace PizzaOrdering
         public string line = null;
 
         //pizza constructor
-        public Pizza(string[]top, int num)
+        public Pizza(string[] top, int num)
         {
             //loop to add toppings to array
             for (int i = 0; i < num; i++)
@@ -24,5 +24,13 @@ namespace PizzaOrdering
             }
             //adjust price based on the number of toppings
             price = 40 + (num * 4);
+        }
+        public double Price { set; get; }
+
+        //method to display the Pizza description and price
+        public override string ToString()
+        {
+            return price + " with " + line + "topping(s)";
+        }
     }
 }
