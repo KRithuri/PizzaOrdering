3a61b6f5007bc860aae8de21e0d80309312032a6
Karabo Rithuri <rithurikarabo@gmail.com>
2024-06-24 13:44:02 +0200

    adjust price based on number of toppings

--- PizzaOrdering/Pizza.cs
+++ PizzaOrdering/Pizza.cs
@@ -9,10 +9,10 @@ namespace PizzaOrdering
     internal class Pizza
     {
         //array to hold pizza flavours
-        private string [] pizzaFlavour = new string [10];
+        private string[] pizzaFlavour = new string[10];
         //array to hold pizza toppings
         private string[] toppings = new string[10];
-        
+
         //data field for price
         private double price;
 
@@ -23,52 +23,62 @@ namespace PizzaOrdering
 
         private string pizz = null;
 
-              public double Price { set; get; }
-
-        public double PizzaPrice { 
-            get { return pizzaPrice; }
-            set { pizzaPrice = value;}
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
         }
-        public string Line{
+
+        public double PizzaPrice
+        {
+            get { return pizzaPrice; }
+            set { pizzaPrice = value; }
+        }
+        public string Line
+        {
             get { return line; }
             set { line = value; }
         }
-        public string Pizz{
+        public string Pizz
+        {
             get { return pizz; }
             set { pizz = value; }
         }
-        public string [] PizzaFlavour{
-            get { return pizzaFlavour;}
-            set { pizzaFlavour = value;}
+        public string[] PizzaFlavour
+        {
+            get { return pizzaFlavour; }
+            set { pizzaFlavour = value; }
         }
-        public string [] Toppings{
-            get { return toppings;}
-            set { toppings = value;}
+        public string[] Toppings
+        {
+            get { return toppings; }
+            set { toppings = value; }
         }
         //pizza constructor
-        public Pizza(string[]pizzaFlavour, int num1,string[] top, int num2)
+        public Pizza(string[] pizzaFlavour, int num, string[] top)
         {
-
             //loop to add pizza flavours to array
-            for(int i = 0; i < num1; i++){
-                pizz += pizzaFlavour[i] + " , ";
-                PizzaPrice += 40;
-            }
-            
-            //loop to add toppings to array
-            for (int i = 0; i < num2; i++)
+            for (int i = 0; i < num; i++)
             {
-                Line += top[i] + " , ";
+                pizz += pizzaFlavour[i] + " , ";
+                pizzaPrice += 40;
+                line += top[i] + " , ";
             }
-            //adjust price based on the number of toppings
-            Price = PizzaPrice + (num2 * 4);
-        }
-  
+            //adjust price based on number of toppings
+            price = pizzaPrice + (num * 4);
 
+            //loop to add toppings to array
+            //for (int i = 0; i < num1; i++)
+            //{
+                //line += top[i] + " , ";
+            //}
+            //adjust price based on the number of toppings
+            //price = pizzaPrice + (num1 * 4);
+        }
         //method to display the Pizza description and price
         public override string ToString()
         {
-            return Pizz +  "flavours\nTotal cost is: R"+Price + " with " + Line + "topping(s)";
+            return pizz + "flavours\nTotal cost is: R" + price + " with " + Line + "topping(s)";
         }
     }
 }
